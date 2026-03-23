a6485f2bbb7de43ad6fb9b5d9bb577c3fe73eb70
Kanzo <im@derg.cc>
2026-03-23 15:40:19 +0900

    Fix URL construction in search and pagination functions

--- static/all_pools.js
+++ static/all_pools.js
@@ -2,22 +2,22 @@ var search_input = document.getElementById('search_input');
 
 function searchPools() {
     var search_value = search_input.value;
-    url = '/' + url_base + '/pools?q=' + search_value;
+    url = url_base + '/pools?q=' + search_value;
     window.location = url;
 }
 
 function next_page() {
     var search_value = search_input.value;
     if (current_page < max_page) {
-        url = '/' + url_base + '/pools?page=' + (current_page + 1) + '&q=' + search_value;
+        url = url_base + '/pools?page=' + (current_page + 1) + '&q=' + search_value;
         window.location = url;
     }
 }
 
-function prev_page() { 
+function prev_page() {
     var search_value = search_input.value;
     if (current_page > 1) {
-        url = '/' + url_base + '/pools?page=' + (current_page - 1) + '&q=' + search_value;
+        url = url_base + '/pools?page=' + (current_page - 1) + '&q=' + search_value;
         window.location = url;
     }
 }
